e199aecbe01f0a084aa120711f75e10d65fcf4b9
Brian Barbour <steelvoltage@gmail.com>
2019-06-10 19:43:03 -0400

    changed footer url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const Footer = () => {
   return (
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="has-text-centered">
         <h6 className="is-size-7">
           <p>
-            Get The Black Hack 2E on{" "}
+            Get The Black Hack 2E on{' '}
             <a
               className="has-text-danger"
               href="https://www.drivethrurpg.com/product/255088/The-Black-Hack-Second-Edition"
@@ -17,13 +17,13 @@ const Footer = () => {
           </p>
           <p>Written by David Black.</p>
           <p>
-            This unofficial fan site was created by{" "}
-            <a href="https://github.com/steelvoltage">steelvoltage</a>.
+            This unofficial fan site was created by{' '}
+            <a href="https://bbarbour.dev">B.BARBOUR</a>.
           </p>
         </h6>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
